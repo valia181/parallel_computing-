aaa42aa2c76d033557e13abe80d4c17fb1e872cc
valalebiga <valalebiga200687@gmail.com>
2026-03-07 10:26:20 +0200

    Lab2 Mutex

--- Lab2/Array.cpp
+++ Lab2/Array.cpp
@@ -1,6 +1,7 @@
 #include "Array.h"
 #include <cstdlib>
 #include <iostream>
+#include <mutex>
 
 Array::Array(int n) : size(n)
 {
@@ -35,14 +36,43 @@ Result Array::findSeq()
         if (data[i] > max) max = data[i];
     }
 
-    int sum = min + max;
-
-    return {min, max, sum};
+    return {min, max, min + max};
 }
 
 Result Array::findMutex(int numThreads)
 {
-    return {0, 0, 0};
+    int min = data[0];
+    int max = data[0];
+    mutex mtx;
+
+    vector<thread> threads;
+
+    auto localWithThreads = [&](int thread_id)
+    {
+        int local_min = data[thread_id];
+        int local_max = data[thread_id];
+        for (int i = thread_id; i < size; i += numThreads)
+        {
+            if (data[i] < local_min) local_min = data[i];
+            if (data[i] > local_max) local_max = data[i];
+        }
+
+        lock_guard<mutex> lock(mtx);
+        if (local_min < min) min = local_min;
+        if (local_max > max) max = local_max;
+    };
+
+    for (int i = 0; i < numThreads; i++)
+    {
+        threads.emplace_back(localWithThreads, i);
+    }
+
+    for (int i = 0; i < numThreads; i++)
+    {
+        threads[i].join();
+    }
+
+    return {min, max, min + max};
 }
 
 Result Array::findCAS(int numThreads)
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
+#include <string>
 #include "Array.cpp"
 
 using namespace std;
+
+void printResults(string nameFunction, Result res, chrono::duration<double, milli> duration)
+{
+    cout << nameFunction << ": " << endl;
+    cout << "Min: " << res.min << endl;
+    cout << "Max: " << res.max << endl;
+    cout << "Sum: " << res.sum << endl;
+    cout << "Time: " << duration.count() << " ms\n" << endl;
+    cout << endl;
+}
 
 int main() {
     int size = 10;
 
     Array arr(size);
 
+    arr.printData();
+    cout << endl;
+
+    // Sequence algorithm
+
     auto start_seq = chrono::high_resolution_clock::now();
 
     Result res_seq = arr.findSeq();
 
-    arr.printData();
-
     auto end_seq = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> duration_seq = end_seq - start_seq;
 
-    cout << "Sequence algorithm: " << endl;
-    cout << "Min: " << res_seq.min << endl;
-    cout << "Max: " << res_seq.max << endl;
-    cout << "Sum: " << res_seq.sum << endl;
-    cout << "Time: " << duration_seq.count() << " ms\n" << endl;
+    printResults("Sequence algorithm", res_seq, duration_seq);
+
+    // Mutex
+    int numThreads = 2;
+    auto start_mutex = chrono::high_resolution_clock::now();
+
+    Result res_mutex = arr.findMutex(numThreads);
+
+    auto end_mutex = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration_mutex = end_mutex - start_mutex;
+
+    printResults("With mutex", res_mutex, duration_mutex);
 
     return 0;
 }
